f4464b1e2cdb1c0eaac31b9ef3a42dd041936448
Martin Alcubierre Arenillas <martin.alcubierre@gmail.com>
2015-01-02 19:21:41 +0100

    Update hockeyapp.js

--- www/hockeyapp.js
+++ www/hockeyapp.js
@@ -4,7 +4,7 @@ var hockeyapp = {
     start:function(success, failure, token) {
         exec(success, failure, "HockeyApp", "start", [ token ]);
     },
-    feedback:function(success, failure) {
+    feedback:function(success, failure, token) {
         exec(success, failure, "HockeyApp", "feedback", [ token ]);
     },
     saveException:function(success, failure, description) {
